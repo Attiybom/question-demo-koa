f400837c555117fa1d012b2d632b80f8c7a62ebe
luminous <910667665@qq.com>
2023-09-14 11:05:27 +0800

    mock + koa study

--- mock/question.js
+++ mock/question.js
@@ -1,53 +1,90 @@
-const Mock = require('mockjs')
+const Mock = require("mockjs");
 
-const Random = Mock.Random
+const Random = Mock.Random;
 
-const getQuestionList = require('../data/getQuestionList')
+const getQuestionList = require("../data/getQuestionList");
 
 module.exports = [
+  // 获取单个问卷信息
   {
-    url: '/api/question/:id',
-    method: 'get',
+    url: "/api/question/:id",
+    method: "get",
     response() {
       return {
         errno: 0,
         data: {
           id: Random.id(),
-          title: Random.ctitle()
-        }
-      }
-    }
+          title: Random.ctitle(),
+        },
+      };
+    },
   },
+  // 创建问卷
   {
-    url: '/api/question',
-    method: 'post',
+    url: "/api/question",
+    method: "post",
     response() {
       return {
         errno: 0,
         data: {
-          id: Random.id()
-        }
-      }
-    }
+          id: Random.id(),
+        },
+      };
+    },
   },
+  // 获取问卷列表
   {
-    url: '/api/question',
-    method: 'get',
+    url: "/api/question",
+    method: "get",
     response(ctx) {
-
       // console.info('ctx', ctx.url)
 
-      const { url } = ctx
-      const isDeleted = url.indexOf('isDeleted=true') >= 0
-      const isStar = url.indexOf('isStar=true') >= 0
+      const { url, query = {} } = ctx;
+      const isDeleted = url.indexOf("isDeleted=true") >= 0;
+      const isStar = url.indexOf("isStar=true") >= 0;
+
+      const pageSize = parseInt(query.pageSize);
 
       return {
         errno: 0,
         data: {
-          list: getQuestionList({isDeleted, isStar}),
-          total: 100 //问卷总数
-        }
-      }
-    }
-  }
-]
+          list: getQuestionList({ len: pageSize, isDeleted, isStar }),
+          total: 100, //问卷总数
+        },
+      };
+    },
+  },
+  // 更新问卷 (由于是mock模拟，所以request暂无要求)
+  {
+    url: "/api/question/:id",
+    method: "patch",
+    response() {
+      return {
+        errno: 0,
+      };
+    },
+  },
+  // 复制问卷
+  {
+    url: "/api/question/copy/:id",
+    method: "post",
+    response() {
+      return {
+        errno: 0,
+        data: {
+          id: Random.id(),
+        },
+      };
+    },
+  },
+  // 批量删除
+  {
+    url: "/api/question/delete",
+    method: "delete",
+    response() {
+      return {
+        errno: 0
+      };
+    },
+  },
+];
